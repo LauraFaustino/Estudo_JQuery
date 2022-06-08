f1677b1545f21a9cad50c20c093361652c4fe9e4
Laura Faustino <laurarfs18@gmail.com>
2022-06-08 15:55:52 -0300

    Atualização javascript_inicial

--- view/Carrinho/javascript_inicial.js
+++ view/Carrinho/javascript_inicial.js
@@ -1,18 +1,30 @@
 // converter texto em número
-function moneyTextToFloat(text){
-    var clearText = text.replace("R$ ", "").replace(",", ".");
-    return parseFloat(clearText);
-}
+function moneyTextToFloat(text) {
+    var cleanText = text.replace("R$ ", "").replace(",", ".");
+    return parseFloat(cleanText);
+    }
 
 // converter número em texto
-function floatToMoneyText(value){
-    var text = (value < 1 ? "0" : "") + Math.floor(value + 100);
+function floatToMoneyText(value) {
+    var text = (value < 1 ? "0" : "") + Math.floor(value * 100);
     text = "R$ " + text;
-    return text.substr(0, text.length -2) + "," + text.substr(-2);
+    return text.substr(0, text.length - 2) + "," + text.substr(-2);
 }
 
-// chamada das funções
+/*var total = document.getElementById("total");
+var formattedText = floatToMoneyText(moneyTextToFloat(total.innerHTML));
+alert(formattedText === total.innerHTML);*/
+
+// ler elemento total
 function readTotal() {
     var total = document.getElementById("total");
     return moneyTextToFloat(total.innerHTML);
 }
+
+// Alterar valor do campo total
+function writeTotal(value) {
+    var total = document.getElementById("total");
+    total.innerHTML = floatToMoneyText(value);
+}
+
+writeTotal(3.14);
