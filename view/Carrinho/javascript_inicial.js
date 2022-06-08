05b429d75c10323d94886770ef85c04404be6d42
Laura Faustino <laurarfs18@gmail.com>
2022-06-08 14:32:04 -0300

    Inicio da configuração do primeiro js em jquery

--- view/Carrinho/javascript_inicial.js
+++ view/Carrinho/javascript_inicial.js
@@ -1,2 +1,17 @@
+// converter texto em número
+function moneyTextToFloat(text){
+    var clearText = text.replace("R$ ", "").replace(",", ".");
+    return parseFloat(clearText);
+}
+
+// converter número em texto
+function floatToMoneyText(value){
+    var text = (value < 1 ? "0" : "") + Math.floor(value + 100);
+    text = "R$ " + text;
+    return text.substr(0, text.length -2) + "," + text.substr(-2);
+}
+
+
 var total = document.getElementById("total");
-alert(total.innetHTML);
+var formattedText = floatToMoneyText(moneyTextToFloat(total.innerHTML));
+alert(formattedText === total.innerHTML);
